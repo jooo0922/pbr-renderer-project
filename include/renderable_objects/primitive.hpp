f2c002d531e194e990c87584d6ac4a93dd402af9
jooo0922 <jooo0922@gmail.com>
2024-09-08 22:28:36 +0900

    feat: Primitive 추상 클래스 구현

---
+++ include/renderable_objects/primitive.hpp
@@ -0,0 +1,28 @@
+#ifndef PRIMITIVE_HPP
+#define PRIMITIVE_HPP
+
+/*
+  #ifndef ~ #endif 전처리기는
+  헤더파일의 헤더가드를 처리하여 중복 include 방지해 줌!
+*/
+
+#include <memory>
+#include "shader/shader.hpp"
+#include "mesh/mesh.hpp"
+#include "renderable_objects/renderable_object.hpp"
+
+class Primitive : public IRenderableObject
+{
+public:
+  ~Primitive() override = default;
+
+protected:
+  Primitive() = default;
+
+  // Mesh 클래스 멤버변수를 스마트 포인터로 관리하는 이유 sphere.cpp 필기 참고
+  std::unique_ptr<Mesh<SimpleVertex>> mesh;
+
+  virtual void generateVertexData(std::vector<SimpleVertex> &vertices, std::vector<unsigned int> &indices) const = 0;
+};
+
+#endif // PRIMITIVE_HPP
