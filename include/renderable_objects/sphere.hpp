0d8a1dddfe05cb25bffb575babc1b9be2e0171bf
jooo0922 <jooo0922@gmail.com>
2024-09-08 22:28:46 +0900

    feat: Sphere 클래스 헤더 구현

---
+++ include/renderable_objects/sphere.hpp
@@ -0,0 +1,26 @@
+#ifndef SPHERE_HPP
+#define SPHERE_HPP
+
+/*
+  #ifndef ~ #endif 전처리기는
+  헤더파일의 헤더가드를 처리하여 중복 include 방지해 줌!
+*/
+
+#include "renderable_objects/primitive.hpp"
+
+class Sphere : public Primitive
+{
+public:
+  // 생성자
+  Sphere();
+
+  // 기본 소멸자
+  ~Sphere() override = default;
+
+  void draw(Shader &shader) override;
+
+protected:
+  void generateVertexData(std::vector<SimpleVertex> &vertices, std::vector<unsigned int> &indices) const override;
+};
+
+#endif // SPHERE_HPP
