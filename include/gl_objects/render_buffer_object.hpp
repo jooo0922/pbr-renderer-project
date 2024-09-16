984557b034411cfc728dfe73fe5b9e94adf4ab96
jooo0922 <jooo0922@gmail.com>
2024-09-16 14:18:45 +0900

    feat: RenderBufferObject 클래스 헤더 선언

---
+++ include/gl_objects/render_buffer_object.hpp
@@ -0,0 +1,38 @@
+#ifndef RENDER_BUFFER_OBJECT_HPP
+#define RENDER_BUFFER_OBJECT_HPP
+
+/*
+  #ifndef ~ #endif 전처리기는
+  헤더파일의 헤더가드를 처리하여 중복 include 방지해 줌!
+*/
+
+#include <glad/glad.h> // OpenGL 함수를 초기화하기 위한 헤더
+#include <gl_objects/gl_object.hpp>
+
+/**
+ * RenderBufferObject 클래스
+ *
+ * offscreen rendering 에 사용할 Renderbuffer 객체를 추상화한 클래스
+ */
+class RenderBufferObject final : public IGLObject
+{
+public:
+  RenderBufferObject();
+
+  ~RenderBufferObject() override;
+
+  void setStorage(GLsizei width, GLsizei height) const;
+
+  GLuint getID() const;
+
+  void bind() const override;
+
+  void unbind() const override;
+
+  void destroy() override;
+
+private:
+  GLuint ID;
+};
+
+#endif // RENDER_BUFFER_OBJECT_HPP
