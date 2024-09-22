5c3b97da0265d473b40a779f54f81bf5a513af6d
jooo0922 <jooo0922@gmail.com>
2024-09-23 01:18:31 +0900

    feat: MaterialFeature 클래스 구현

---
+++ src/features/material_feature.cpp
@@ -0,0 +1,77 @@
+#include "features/material_feature.hpp"
+
+MaterialFeature::MaterialFeature()
+    : pbrShaderPtr(nullptr),
+      roughness(0.5f),
+      metallic(0.0f),
+      ambientOcclusion(1.0f),
+      albedo(glm::vec3(0.5f, 0.0f, 0.0f))
+{
+}
+
+void MaterialFeature::initialize()
+{
+}
+
+void MaterialFeature::process()
+{
+  pbrShaderPtr->use();
+
+  pbrShaderPtr->setFloat("roughness", roughness);
+  pbrShaderPtr->setFloat("metallic", metallic);
+  pbrShaderPtr->setFloat("ao", ambientOcclusion);
+  pbrShaderPtr->setVec3("albedo", albedo);
+}
+
+void MaterialFeature::finalize()
+{
+  pbrShaderPtr = nullptr;
+}
+
+void MaterialFeature::onChange(const MaterialParameter &param)
+{
+  if (roughness != param.roughness)
+  {
+    setRoughness(param.roughness);
+  }
+
+  if (metallic != param.metallic)
+  {
+    setMetallic(param.metallic);
+  }
+
+  if (ambientOcclusion != param.ambientOcclusion)
+  {
+    setAmbientOcclusion(param.ambientOcclusion);
+  }
+
+  if (albedo != param.albedo)
+  {
+    setAlbedo(param.albedo);
+  }
+}
+
+void MaterialFeature::setPbrShader(Shader *pbrShader)
+{
+  pbrShaderPtr = pbrShader;
+}
+
+void MaterialFeature::setRoughness(const float value)
+{
+  roughness = value;
+}
+
+void MaterialFeature::setMetallic(const float value)
+{
+  metallic = value;
+}
+
+void MaterialFeature::setAmbientOcclusion(const float value)
+{
+  ambientOcclusion = value;
+}
+
+void MaterialFeature::setAlbedo(const glm::vec3 &color)
+{
+  albedo = color;
+}
