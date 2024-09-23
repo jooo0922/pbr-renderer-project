0fae556436be6460b82826427d7ecad7108a9415
jooo0922 <jooo0922@gmail.com>
2024-09-23 22:48:56 +0900

    comment: 주석 추가

--- src/features/material_feature.cpp
+++ src/features/material_feature.cpp
@@ -4,7 +4,7 @@ MaterialFeature::MaterialFeature()
     : pbrShaderPtr(nullptr),
       roughness(0.5f),
       metallic(0.0f),
-      ambientOcclusion(1.0f),
+      ambientOcclusion(1.0f), // 각 프래그먼트의 ambient occlusion(환경광 차폐) factor 를 1로 지정 -> 즉, 환경광이 차폐되는 영역이 없음!
       albedo(glm::vec3(0.5f, 0.0f, 0.0f))
 {
 }
@@ -20,7 +20,7 @@ void MaterialFeature::process()
   pbrShaderPtr->setFloat("roughness", roughness);
   pbrShaderPtr->setFloat("metallic", metallic);
   pbrShaderPtr->setFloat("ao", ambientOcclusion);
-  pbrShaderPtr->setVec3("albedo", albedo);
+  pbrShaderPtr->setVec3("albedo", albedo); // 표면 밖으로 빠져나온 diffuse light 색상값을 쉐이더 프로그램에 전송
 }
 
 void MaterialFeature::finalize()
