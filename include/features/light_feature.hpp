a34186c99c1bdd934e941202e8791cc76c1d243a
jooo0922 <jooo0922@gmail.com>
2024-10-06 00:23:57 +0900

    feat: LightFeature 클래스 헤더 선언

---
+++ include/features/light_feature.hpp
@@ -0,0 +1,62 @@
+#ifndef DIRECT_LIGHT_FEATURE_HPP
+#define DIRECT_LIGHT_FEATURE_HPP
+
+/*
+  #ifndef ~ #endif 전처리기는
+  헤더파일의 헤더가드를 처리하여 중복 include 방지해 줌!
+*/
+
+#include <memory>
+#include <array>
+#include <glm/glm.hpp>
+#include <features/feature.hpp>
+#include <common/listener.hpp>
+#include <shader/shader.hpp>
+#include <light/light.hpp>
+
+struct LightData
+{
+  glm::vec3 position;
+  glm::vec3 color;
+};
+
+struct LightParameter
+{
+  std::array<LightData, LightFeature::numLights> lightDataArray;
+};
+
+/**
+ * LightFeature 클래스
+ *
+ * 직접광(= direct light) 관련 파라미터들을 관리하는 Feature 클래스
+ */
+class LightFeature : public IFeature, public IListener<LightParameter>
+{
+public:
+  static constexpr unsigned int numLights = 4;
+
+  LightFeature();
+
+  void initialize() override;
+  void process() override;
+  void finalize() override;
+
+  void onChange(const LightParameter &param) override;
+
+  void setPbrShader(std::shared_ptr<Shader> pbrShader);
+
+  void getLightParameter(LightParameter &param) const;
+
+private:
+  std::array<Light, numLights> lights;
+
+  std::shared_ptr<Shader> pbrShaderPtr;
+
+  LightParameter lightParameter;
+
+  // 파라미터 Setter 멤버 함수
+  void setPosition(Light &light, const glm::vec3 &position);
+  void setColor(Light &light, const glm::vec3 &color);
+};
+
+#endif // DIRECT_LIGHT_FEATURE_HPP
