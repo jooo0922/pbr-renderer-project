8d7cd5800b99f6b04e2529047c087214eb0114f7
jooo0922 <jooo0922@gmail.com>
2024-10-06 17:32:44 +0900

    feat: LightUi 클래스 헤더 구현

---
+++ include/ui_containers/light_ui.hpp
@@ -0,0 +1,42 @@
+#ifndef LIGHT_UI_HPP
+#define LIGHT_UI_HPP
+
+/*
+  #ifndef ~ #endif 전처리기는
+  헤더파일의 헤더가드를 처리하여 중복 include 방지해 줌!
+*/
+
+#include "features/light_feature.hpp"
+#include "ui_components/drag_float3.hpp"
+#include "ui_components/color_edit.hpp"
+#include "ui_components/combo.hpp"
+
+struct LightUiSet
+{
+  DragFloat3 position;
+  ColorEdit color;
+};
+
+/**
+ * LightUi 클래스
+ *
+ * camera 관련 파라미터들의 UI 입력을 처리하는
+ * UiComponent 요소들을 관리하는 UI 컨테이너 클래스
+ */
+class LightUi : public IListener<LightParameter>
+{
+public:
+  LightUi();
+  ~LightUi();
+
+  bool onUiComponents();
+  void onChange(const LightParameter &param) override;
+
+  void getLightParam(LightParameter &param) const;
+
+private:
+  Combo lightSelector;
+  std::array<LightUiSet, LightConstants::NUM_LIGHTS> uiList;
+};
+
+#endif // LIGHT_UI_HPP
