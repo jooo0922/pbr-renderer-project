eae7a13bba39f4faf842a4182bccea5cdb07d521
jooo0922 <jooo0922@gmail.com>
2024-10-06 18:06:03 +0900

    feat: intensity getter 및 setter 구현

--- src/light/light.cpp
+++ src/light/light.cpp
@@ -1,9 +1,10 @@
 #include "light/light.hpp"
 
-Light::Light(const glm::vec3 &position, const glm::vec3 &color)
+Light::Light(const glm::vec3 &position, const glm::vec3 &color, const float &intensity)
 {
   this->position = position;
   this->color = color;
+  this->intensity = intensity;
 }
 
 const glm::vec3 &Light::getPosition() const
@@ -16,6 +17,11 @@ const glm::vec3 &Light::getColor() const
   return color;
 }
 
+const float &Light::getIntensity() const
+{
+  return intensity;
+}
+
 void Light::setPosition(const glm::vec3 &position)
 {
   this->position = position;
@@ -24,4 +30,9 @@ void Light::setPosition(const glm::vec3 &position)
 void Light::setColor(const glm::vec3 &color)
 {
   this->color = color;
+}
+
+void Light::setIntensity(const float &intensity)
+{
+  this->intensity = intensity;
 }
