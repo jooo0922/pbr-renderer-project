c2de05a878d93c5042c4cfa623ddc2760ba1c3de
jooo0922 <jooo0922@gmail.com>
2024-10-30 00:34:48 +0900

    feat: 모델링 파일 url 저장 컨테이너 멤버 변수 추가

--- include/features/model_feature.hpp
+++ include/features/model_feature.hpp
@@ -12,7 +12,7 @@
 struct ModelParameter
 {
   glm::vec3 position;
-  glm::vec3 rotation;
+  glm::vec3 rotation; // rotation 파라미터는 Euler 각 기준 인터페이스 정의 -> 내부에서는 Quaternion 으로 계산
   glm::vec3 scale;
   size_t modelIndex;
 };
@@ -49,6 +49,9 @@ private:
 
   // ImGui 로부터 입력받은 position, rotation, scale 값으로 계산할 모델 행렬
   glm::mat4 transform;
+
+  // 모델링 파일 url 을 저장할 컨테이너
+  std::array<const char *, ModelConstants::NUM_MODELS> modelUrls;
 
   // Model 인스턴스를 저장할 정적 배열 컨테이너
   std::array<std::unique_ptr<Model>, ModelConstants::NUM_MODELS> models;
